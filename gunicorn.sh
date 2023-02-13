44a037643a27aaf7dbf5d7f9409df771a7832663
Anaghatmohan <111461738+Anaghatmohan@users.noreply.github.com>
2023-02-13 21:50:00 +0100

    gunicorn setup

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
 source myenv/bin/activate
+
+cd $WORKSPACE/djangoproject
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "DB related changes are done"
+
+cd $WORKSPACE
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
+
