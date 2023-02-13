b799bb7339863cb1598b0eed68b260d3675af3e2
Anagha <anaghatmohan@gmail.com>
2023-02-13 20:09:22 +0530

    'changes'

---
+++ envsetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [-d "myenv"]
+then
+    echo "Python virtual env exists"
+else
+    python virtualenv myenv
+
+fi
+
+echo $PWD
+source myenv/Scripts/activate
+
+pip install -r requirements.txt
+
+if [-d "logs"]
+then
+    echo "log folder exists"
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
+
+echo "env setup done"
